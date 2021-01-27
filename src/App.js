7133f09b1c418d4dab8d84cdab1faed4c136d30c
Muhammad Mahmoud Agha <muha.agha@gmail.com>
2021-01-27 12:44:45 +0100

    first commant

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+<div  style={{margin:' 60px auto', width:'340px' , display:'block', textAlign:'center'}}>
+<h2> أهلا بك في موقع معرفة </h2>
+<h3> سننطلق قريباُ, نريدك ان تكون معنا على صفحة <a href="https://fb.me/www.Marifa.net">الفيسبوك</a> </h3>
+</div>
+  );   
+};
+
+export default App;
