9e2e5f480787ab211bfb7694611e325d2327a117
Muhammad Mahmoud Agha <muha.agha@gmail.com>
2021-02-14 12:17:15 +0100

    section card desktop

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,17 +2,18 @@ import React from 'react';
 import HeroImage from './components/heroImage/HeroImage';
 import SectionCard from '../../shared/sectionCard/SectionCard';
 
+
 const Home = () => {
     return (
         <>
             <HeroImage/>
            <div className="sections-cards container">
-               <SectionCard title="تنمية بشرية" content="2"/>
-               <SectionCard title="علوم سياسية" content="20"/>
-               <SectionCard title="إدارة أعمال" content="51"/>
-               <SectionCard title="تربية أطفال" content="9"/>
-               <SectionCard title="روايات ادبية" content="19"/>
-               <SectionCard title="عرض كل الأقسام" content="2"/>
+               <SectionCard title="تنمية بشرية" content="2" icon="Human_development"/>
+               <SectionCard title="علوم سياسية" content="20" icon="Political_science"/>
+               <SectionCard title="إدارة أعمال" content="51" icon="Business_administration"/>
+               <SectionCard title="عرض كل الأقسام" content="8" icon="no"/>
+               <SectionCard title="تربية أطفال" content="9" icon="Child_rearing"/>
+               <SectionCard title="روايات ادبية" content="19" icon="Novels"/>
            </div>
         </>
     )
--- src/shared/sectionCard/SectionCard.js
+++ src/shared/sectionCard/SectionCard.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import {SECTION_CARD_LOCA} from '../../localization/ar/Language';
+import Business_administration from './svg/business_administration.svg';
+import Child_rearing from './svg/child_rearing.svg';
+import Human_development from './svg/human_development.svg';
+import Novels from './svg/novels.svg';
+import Political_science from './svg/political_science.svg';
 
-const SectionCard = ({title, content}) => {
+const SectionCard = ({title, content,icon}) => {
+
+let cartIcon
+
+    switch (icon) {
+        case 'Business_administration':
+            cartIcon =  <img  src={Business_administration} alt={"marifa.de section" + title} />
+            break;
+        case 'Child_rearing':
+            cartIcon =  <img  src={Child_rearing} alt={"marifa.de section" + title} />
+            break;
+        case 'Human_development':
+            cartIcon =  <img  src={Human_development} alt={"marifa.de section" + title} />
+            break;
+        case 'Novels':
+            cartIcon =  <img  src={Novels} alt={"marifa.de section" + title} />
+            break;
+        case 'no':
+            cartIcon =  ''
+            break;
+        default:
+            cartIcon =  <img  src={Political_science} alt={"marifa.de section" + title} />
+            break;
+    }
+ 
     return (
-        <div className="section-card">
-            <div className="section-card_info">
+        <div className={icon === 'no'? "section-card section-card-center": "section-card"}>
+            <div className={icon === 'no'? "section-card_info section-card_info-center": "section-card_info"}>
             <span className="section-card_title">{title}</span>
-            <span className="section-card_content"> {SECTION_CARD_LOCA.thereIs} <i>{content}</i> {SECTION_CARD_LOCA.ToIncreaseYourKnowledge}</span>
+                 <span className="section-card_content">
+                 {SECTION_CARD_LOCA.thereIs} <i> {content} </i>
+                 {icon === 'no'? 
+                 SECTION_CARD_LOCA.sections + SECTION_CARD_LOCA.ToIncreaseYourKnowledges 
+                 : SECTION_CARD_LOCA.book + SECTION_CARD_LOCA.ToIncreaseYourKnowledge}
+                 </span>
             </div>
-            <div className="section-card_icon material-icons">done</div>
+            {icon === 'no'? "": <div className="section-card_icon" >{cartIcon}</div>}
+            
         </div>
     )
 }
