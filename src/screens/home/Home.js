130c81f92b6c0dd30dcd6a05a827edfea29d1507
Muhammad Mahmoud Agha <muha.agha@gmail.com>
2021-02-18 12:38:27 +0100

    book card desktop

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -25,17 +25,17 @@ const Home = () => {
                <h3>{HOME_PAGE_LOCA.VariousBooksH3}</h3>
                </div>
                <div className="books-cards">
-               <BookCard/>
-               <BookCard/>
-               <BookCard/>
-               <BookCard/>
-               <BookCard/>
+               <BookCard image="test" title='قاعدة الثواني الخمس' author='ميل روبنز' price='15€' salePrice='13,99€' stars='5'/>
+               <BookCard image="test" title='فن العيش الحكيم' author='أرتور شوبنهاور' price='16€' salePrice='11,99€' stars='5'/>
+               <BookCard image="test" title='الخطة السرية لإنقاذ البشرية' author='احمد خير العمري' price='8€' salePrice='6,99€' stars='5'/>
+               <BookCard image="test" title='مئة عام من العزلة' author='غابرييل غارسيا ماركيز' price='35€' salePrice='20,99€' stars='5'/>
+               <BookCard image="test" title='الخيميائي' author='باولو كويلو' price='10€' salePrice='6,99€' stars='5'/>
               
-               <BookCard/>
-               <BookCard/>
-               <BookCard/>
-               <BookCard/>
-               <BookCard/>
+               <BookCard image="test" title='28 حرف' author='أحمد ذكي' price='15€' salePrice='13,99€' stars='5'/>
+               <BookCard image="test" title='مميز بالأصفر' author='جاكسون براون' price='8€' salePrice='6,99€' stars='5'/>
+               <BookCard image="test" title='حلم رجل مضحك' author='دوستويفسكي' price='25€' salePrice='20,99€' stars='5'/>
+               <BookCard image="test" title='قوى العقل الباطن' author='جوزيف ميرفي' price='13€' salePrice='9,99€' stars='5'/>
+               <BookCard image="test" title='مزرعة الحيوان' author='جورج اوريل' price='11€' salePrice='10,99€' stars='5'/>
                </div>
 
            </div>
--- src/shared/bookCard/BookCard.js
+++ src/shared/bookCard/BookCard.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
-const BookCard = ({title='عنوان الكتاب', author='اسم الكاتب', price='10€', stars='5'}) => {
+const BookCard = ({image='test',title='عنوان الكتاب', author='اسم الكاتب', price='15€', salePrice='13,99€', stars='5'}) => {
     return (
         <div className='book-card'>
-            <div className='book-card_image'></div>
+            <div className='book-card_image'>
+                <img src={require('../../media/images/'+image+'.png')} />
+            </div>
             <div className='book-card_title'>
                 <p>{title}</p>
                 <span>{author}</span>
             </div>
             <div className='book-card_footer'>
-                 <span className='book-card_footer-stars'>{stars}</span>
-                 <span className='book-card_footer-price'>{price}</span>
+                 <div className='book-card_footer-stars'>
+                     <i className="material-icons md-20">star_outline</i>
+                     <i className="material-icons md-20">star_outline</i>
+                     <i className="material-icons md-20">star_half</i>
+                     <i className="material-icons md-20">star</i>
+                     <i className="material-icons md-20">star</i>
+                 </div> 
+                 <div className='book-card_footer-price'>
+                   
+                 <span className="book-card_footer-price_sale">{salePrice}</span> 
+                     <span className="book-card_footer-price_normal">
+                         <i className="book-card_footer-price_normal-with-sale"></i>
+                         {price}
+                     </span>
+
+                 </div>
             </div>
         </div>
     )
